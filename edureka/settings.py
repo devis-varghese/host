44bf16b8f1e8fcdca517ddf9ce7c4c50dc315a16
devis varghese <>
2023-05-23 20:18:25 +0530

    updates

--- edureka/settings.py
+++ edureka/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'edureka.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mincc',
+        'PORT':'5432',
+        'PASSWORD': "AsRJ4pDWUitdXB9slrHOV3gNXWLLLwIV",
+        'USER': "mincc",
+        'HOST': "dpg-chlr3hu4dadfmsi6e7ug-a.oregon-postgres.render.com"
 
     }
 }
--- posts/models.py
+++ posts/models.py
@@ -256,11 +256,9 @@ class video(models.Model):
     title = models.CharField(max_length=100, null=False)
     post = post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='videos')
     serial_number = models.IntegerField(null=False)
-    # video_id = models.CharField(max_length=100)
-    video_id = EmbedVideoField()
-
     is_preview = models.BooleanField(default=False)
     desc = RichTextField(blank=True, null=True)
+    # video_id = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
